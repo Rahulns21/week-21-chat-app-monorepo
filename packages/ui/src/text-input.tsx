d66817726b2c90f1b7309c21d3d242b14270579c
Rahul NS <rahul9380767545@gmail.com>
2025-03-22 17:59:40 +0530

    UI: Add text-input component

---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,16 @@
+interface PropType {
+    placeholder: string;
+    size: "small" | "big";
+}
+
+export function TextInput({
+    placeholder,
+    size,
+}: PropType) {
+    return <input placeholder={placeholder} style={{
+        padding: size === "big" ? 20 : 10,
+        margin: size === "big" ? 20 : 10,
+        borderColor: "black",
+        borderWidth: 1,
+    }}></input>
+}
